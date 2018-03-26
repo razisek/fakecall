d2e7849a35cbe0efa420393c086c49cbac952ae7
Azis <33994563+R1iaz@users.noreply.github.com>
2018-03-26 16:45:09 +0700

    Update fakecall.py

--- fakecall.py
+++ fakecall.py
@@ -1,4 +1,4 @@
-##sorce : https://github.com/Gameye98/
+##sorce : Gameye98
 import sys
 import time
 import requests
@@ -6,8 +6,8 @@ import requests
 __banner__ = """
 \033[1;33m   ____     __         _____     ____ \033[1;37m| Author : Mr.r1i14z
 \033[1;33m  / __/__ _/ /_____   / ___/__ _/ / / \033[1;37m| Contact : azisek@merahputih.id
-\033[1;33m / _// _ `/  '_/ -_) / /__/ _ `/ / /  \033[1;37m| github : \033[4mhttps://github.com/R1iaz\033[0m'
-\033[1;33m/_/  \_,_/_/\_\\__/   \___/\_,_/_/_/   \033[1;37m| Never Stop To Learning
+\033[1;33m / _// _ `/  '_/ -_) / /__/ _ `/ / /  \033[1;37m| github : \033[4mhttps://github.com/R1iaz\033[0m
+\033[1;33m/_/  \_,_/_/\_\\__/  \___/\_,_/_/_/   \033[1;37m| Never Stop To Learning
 """
 
 def spam():
@@ -25,7 +25,7 @@ def spam():
 				print("\033[1;31m[FAILED] Gagal Dikirim :(... Tunggu 60 Detik...\033[0m")
 			time.sleep(60)
 			count = count + 1
-		print("\033[1;36m[ DONE ] Stopped...")
+		print("\033[1;36m[ DONE ] Exiting...")
 		sys.exit(1)
 	else:
 		print __banner__
